75e0bb6111ed199df1ba9ecb3d44249af6baaa35
ArmandoPeralta0405 <armando.peralta.0405@gmail.com>
2023-09-15 16:56:28 -0400

    Fecha 15/09/2023 16:56

---
+++ src/dbConfig.ts
@@ -0,0 +1,9 @@
+// src/dbConfig.ts
+
+export const dbConfig = {
+    host: 'localhost', // Cambia esto al host de tu base de datos
+    user: 'root', // Cambia esto a tu nombre de usuario de MariaDB
+    password: '', // Cambia esto a tu contraseña de MariaDB
+    database: 'clinicaveterinariabd', // Cambia esto al nombre de tu base de datos
+    port: 3306
+};
